ba7ebe179badb7c24114e41cfd96fdb3b9a3d2d6
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-12-22 16:41:44 -0300

    Add files via upload
    casi responsive . Agregado el menu y el btn menu

---
+++ agenda_telefonica_frontend/scripts.js
@@ -0,0 +1,26 @@
+console.log("hola console");
+
+const $d = document;
+
+
+const mostrarMenu = (idMenu, classShow) => {
+    $d.getElementById(idMenu).classList.add(classShow);
+}
+
+const ocultarMenu = (idMenu, classShow) => {
+    $d.getElementById(idMenu).classList.remove(classShow);
+}
+
+$d.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.id == 'btn-menu'){
+        console.log("click en el btn menu");
+        mostrarMenu('navbar', 'navbar-show');
+    }    
+    if (e.target.id == 'btn-close' || e.target.id.includes('navbar_enlace')){
+        console.log('click en el btn cerrar menu');        
+        ocultarMenu('navbar', 'navbar-show');        
+    }        
+});
+
+
