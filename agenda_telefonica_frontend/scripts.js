b86484c634e0954d71de599ac8918bfa8197b5df
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-01-20 16:32:25 -0300

    Add files via upload
    ver contactos casi terminado con grid

--- agenda_telefonica_frontend/scripts.js
+++ agenda_telefonica_frontend/scripts.js
@@ -1,3 +1,4 @@
+/*
 console.log("hola console");
 
 const $d = document;
@@ -23,4 +24,50 @@ $d.addEventListener('click', (e) => {
     }        
 });
 
+*/
 
+
+
+/*
+Primera parte:
+Hacer una funcion que reciba dos valores del tipo 'number' como argumentos.
+Tiene que controlar que sean del tipo 'number' y debe mostrar el error.
+Tiene que incrementar el contador con el incremento pasados.
+Tiene que retornar ese incremento.
+
+Segunda parte:
+Dado el array: precios = [98, 149, 34, 78, 255, 15]
+Hacer un ciclo for e incremente todos los elementos con un valor fijo de 10
+*/
+function incrementar(contador, incremento){
+    let resultado = null;
+    resultado = contador + incremento;
+    return resultado;
+}
+
+
+
+/*
+Hacer una funcion que reciba dos valores como argumentos.
+Tiene que controlar que sean del tipo 'number' y debe mostrar el error.
+Tiene que devolver la division entre el primero y el segundo.
+Si el segundo es igual a cero, no se puede hacer la division y debe mostrar el error.
+Tiene que retornar el valor de dicha division.
+*/
+function dividir(dividendo, divisor){
+    let resultado = null;
+    if (typeof dividendo == 'number' && typeof divisor == 'number'){
+        if (divisor !== 0){
+            resultado = dividendo / divisor;
+        }else{
+            resultado = "El divisor debe ser distinto a cero";
+        }
+    }else{
+        resultado = "Los valores deber ser del tipo 'numero'";
+    }
+    return resultado;
+}
+
+console.log(dividir(5, 2));
+console.log(dividir(5, '2'));
+console.log(dividir(5, 0));
