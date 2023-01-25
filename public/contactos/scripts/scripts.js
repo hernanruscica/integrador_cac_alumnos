a186fbba373fe7b06c89f27cb152e1438ea63f74
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-01-25 14:12:01 -0300

    Add files via upload
    vista previa en la carga de imagenes y los botones de las tarjetas que hacen click con la accion y el id

--- public/contactos/scripts/scripts.js
+++ public/contactos/scripts/scripts.js
@@ -3,5 +3,31 @@ const $d = document;
 
 
 $d.addEventListener('click', (e) => {    
-    console.log(e.target.id);
+        if (e.target.matches(".contactos-tarjeta__titulo__botones__btn")) {
+            console.log(`boton clickeado\naccion: ${e.target.id} - id: ${e.target.dataset.id}`);
+        }
+        if (e.target.matches(".contactos-tarjeta__titulo__botones__btn i")) {
+            console.log(`boton clickeado\naccion: ${e.target.parentElement.id} - id: ${e.target.parentElement.dataset.id}`);
+        }
+        if (e.target.matches(".contactos-tarjeta__titulo__botones__btn span")){
+            console.log(`boton clickeado\naccion: ${e.target.parentElement.id} - id: ${e.target.parentElement.dataset.id}`);
+        }            
+    
 })
+
+$d.addEventListener('change', function(event) {
+    var target = event.target;
+    if (target.id == "foto-perfil") {
+      var preview = $d.getElementById('preview');
+      var file = target.files[0];
+      var reader = new FileReader();
+  
+      reader.addEventListener("load", function () {
+        preview.src = reader.result;
+      }, false);
+  
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+  });
--- routes/contactosRoutes.js
+++ routes/contactosRoutes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/')
+        cb(null, 'public/imgs')
       },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname)
