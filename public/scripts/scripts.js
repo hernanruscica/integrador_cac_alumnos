c15f1f2a00b42863414a2b1c9b2cee76b5e9867b
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-02-03 16:14:44 -0300

    Add files via upload
    casi todas las validaciones del lado del cliente

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,29 @@
 console.log("desde public/scripts.js")
+const $d = document;
+let contraseniasCorrectas = null;
+
+
+$d.addEventListener("keyup", (e) => {
+    if (e.target.id == "contrasenia2" || e.target.id == "contrasenia"){
+        let contrasenia = $d.getElementById("contrasenia");
+        let contrasenia2 =  $d.getElementById("contrasenia2");
+        if (contrasenia.value == contrasenia2.value){
+            //formulario__campo__error-oculto
+            contrasenia.nextElementSibling.classList.add('formulario__campo__error-oculto');
+            contrasenia2.nextElementSibling.classList.add('formulario__campo__error-oculto');
+            contraseniasCorrectas = true;
+            console.log("contraseñas iguales");
+        }else{
+            contrasenia.nextElementSibling.classList.remove('formulario__campo__error-oculto');
+            contrasenia2.nextElementSibling.classList.remove('formulario__campo__error-oculto');
+            console.log("contraseñas distintas");
+            contraseniasCorrectas = false;
+        }        
+    }
+})
+$d.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    if (contraseniasCorrectas == true){
+        e.target.submit();
+    }
+})
